3f75a22320a517f22b81c0243495248b88305484
GouriiNandanaa <150128982+GouriiNandanaa@users.noreply.github.com>
2025-09-02 08:04:07 +0530

    Add files via upload

---
+++ study/free-code-camp/modules.js
@@ -0,0 +1,8 @@
+const names = require('./names')
+const sayHi = require('./utils')
+console.log(typeof(sayHi))
+console.log(names.john)
+
+sayHi(names.peter)
+
+const sumCalcu = require('./7-mind-games')
---
+++ study/free-code-camp/utils.js
@@ -0,0 +1,5 @@
+const sayHi = (name) => {console.log(`Hello ${name}`)};
+sayHi('jithin')
+console.log(typeof(sayHi))
+
+module.exports = sayHi
